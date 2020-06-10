748f0e04d54875bc5e427a69e6d5070ce7229dce
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-06-10 14:24:43 -0500

    fixed axios error

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
+
+
 export const FETCH_FOXES = 'FETCH_FOXES';
 
 export const getFoxes = () => dispatch => {
-  return {
-    dispatch ({type: FETCH_FOXES});
-  };
+
+    dispatch({ type: FETCH_FOXES });
+    axios
+      .get('https://randomfox.ca/floof')
+      .then(res => {
+        console.log(res);
+        dispatch({  type: UPDATE_FOXES, payload: res.data });
+      })
+      .catch(err => console.log('There was an error fetching foxes', err));
+   
 };
